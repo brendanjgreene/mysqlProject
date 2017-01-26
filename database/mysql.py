1e020e6943d5b3ab66653ab4009fb465607ae6a7
Brendan Greene <brendanjgreene@gmail.com>
2017-01-26 14:50:17 +0000

    unignore databse files

---
+++ database/mysql.py
@@ -0,0 +1,9 @@
+import MySQLdb as _mysql
+
+# Connection details for `mydb` database
+db = _mysql.connect(
+    db='mydb',
+    host='localhost',
+    user='root',
+    passwd='MySQLWorkbench1971'
+)
