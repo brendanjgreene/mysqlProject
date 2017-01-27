22c2c8bdb2d54a2a2f520eeae997f2133e4afa3c
Brendan Greene <brendanjgreene@gmail.com>
2017-01-27 17:20:03 +0000

    using select including converting to named tuples

--- database/mysql.py
+++ database/mysql.py
@@ -1,4 +1,5 @@
 import MySQLdb as _mysql
+from collections import namedtuple
 
 
 class MySQLDatabase(object):
@@ -65,3 +66,62 @@ class MySQLDatabase(object):
         cursor.close()
 
         return self.columns
+
+    def convert_to_named_tuples(self, cursor):
+        results = None
+        names = " ".join(d[0] for d in cursor.description)
+        klass = namedtuple('Results', names)
+
+        try:
+            results = map(klass._make, cursor.fetchall())
+        except _mysql.ProgrammingError, e:
+            print e
+
+        return results
+
+    def select(self, table, columns=None, named_tuples=False, **kwargs):
+        """
+        We'll create our `select` method in order
+        to make it simpler for extracting data from
+        the database.
+        select(table_name, [list_of_column_names])
+        """
+        sql_str = "SELECT "
+
+        # add columns or just use the wildcard
+        if not columns:
+            sql_str += " * "
+        else:
+            for column in columns:
+                sql_str += "%s, " % column
+
+            sql_str = sql_str[:-2]  # remove the last comma!
+
+        # add the table to the SELECT query
+        sql_str += " FROM `%s`.`%s`" % (self.database_name, table)
+
+        # if there's a JOIN clause attached
+        if kwargs.has_key('join'):
+            sql_str += " JOIN %s" % kwargs.get('join')
+
+        # if there's a WHERE clause attached
+        if kwargs.has_key('where'):
+            sql_str += " WHERE %s " % kwargs.get('where')
+
+        # if there's a LIMIT clause attached
+        if kwargs.has_key('limit'):
+            sql_str += " LIMIT %s" % kwargs.get('limit')
+
+        sql_str += ";"  # Finalise out SQL string
+
+        cursor = self.db.cursor()
+        cursor.execute(sql_str)
+
+        if named_tuples:
+            results = self.convert_to_named_tuples(cursor)
+        else:
+            results = cursor.fetchall()
+
+        cursor.close()
+
+        return results
--- main.py
+++ main.py
@@ -30,3 +30,43 @@ print columnsOrders
 
 columnsArticles = db.get_columns_for_table('articles')
 print columnsArticles
+
+# Get all the records from
+# the people table
+all_records = db.select('people')
+print "All records: %s" % str(all_records)
+
+# Get all of the records from
+# the people table but only the
+# `id` and `first_name` columns
+column_specific_records = db.select('people', ['id', 'first_name'])
+print "Column specific records: %s" % str(column_specific_records)
+
+# Select data using the WHERE clause
+where_expression_records = db.select('people', ['first_name'],
+                                     where="first_name='John'")
+print "Where Records: %s" % str(where_expression_records)
+
+# Select data using the WHERE clause and
+# the JOIN clause
+joined_records = db.select('people', ['first_name'],
+                           where="people.id=3",
+                           join="orders ON people.id=orders.person_id")
+print "Joined records: %s" % str(joined_records)
+
+# Select using the limit clause
+limited_results = db.select('orders', limit='5')
+print "--------------------------------------"
+print "First 5 Results"
+print "--------------------------------------"
+# iterate over the list of results
+for result in limited_results:
+	print result
+print "--------------------------------------"
+
+# Limit the results to 10
+limited_results = db.select('orders', limit='10')
+print "First 10 results"
+print "--------------------------------------"
+for result in limited_results:
+	print result
