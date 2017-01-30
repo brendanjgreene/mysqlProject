4f693d4d7a3ec3bcb0163ddac8051a984aa95aa7
Brendan Greene <brendanjgreene@gmail.com>
2017-01-30 12:02:12 +0000

    challenge B and C complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from database.mysql import MySQLDatabase
 from settings import db_config
+import random
 
 """
 Retrieve the settings from the
@@ -64,4 +65,35 @@ peopleavg = db.select('people', columns=["CONCAT(first_name, ' ', second_name)" 
 
 for person in peopleavg:
     print person.full_name, "spent on average ", person.avg_spend
+
+# Challenge B:
+# Create a new person in the people table and
+# add in a profile row and two orders of random value.
+
+db.insert('people', first_name="Caroline", second_name="Greene", DOB='STR_TO_DATE("27-11-1971", "%d-%m-%Y")')
+caroline = db.select('people', ["id", 'first_name'], where='first_name="Caroline"', named_tuples=True)[0]
+db.insert('profiles', person_id="%s" % caroline.id, address="Turnings, Straffan")
+db.insert('orders', person_id="%s" % caroline.id, amount='%s' % random.randint(1,30))
+db.insert('orders', person_id="%s" % caroline.id, amount='%s' % random.randint(5,67))
+orders = db.select('orders', where='person_id=%s' %caroline.id)
+
+for order in orders:
+    print order
+
+# Challenge C:
+# Once youve added them in use select to get
+# their full name and the minimum amount they have spent.
+
+personCaroline = db.select('people', columns=["CONCAT(first_name, ' ', second_name)"
+                                      " AS full_name", "MIN(amount)"
+                                      " AS min_spend"],
+                   named_tuples=True, where="people.first_name='Caroline'",
+                   join="orders ON people.id=orders.person_id")
+
+print personCaroline
+
+# Challenge D:
+# Choose a person and update ALL of his orders to have the amount 20.02.
+
+
 
